6f320cdbbcbf687cd255f84b6ac1a354e5c5a015
Lekan Olaoye <leocode425@gmail.com>
2022-04-07 18:46:07 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
+
+
 /**
- * main - prints the phrase with proper grammar,
- * Return: 0 if exited property, non-zero otherwise
- */
+
+* main - prints the phrase with proper grammar,
+
+* Return: 0 if exited property, non-zero otherwise
+
+*/
+
 int main(void)
+
 {
+
         printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+        return (0);
+
 }
