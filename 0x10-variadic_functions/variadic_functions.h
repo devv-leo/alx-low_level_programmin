7d86bbe5391b119dfed1302fb125a44ec3a9a93f
LËØ <87527071+devv-leo@users.noreply.github.com>
2022-05-21 18:14:30 +0100

    Create variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef FUNC_H
+#define FUNC_H
+int sum_them_all(const unsigned int n, ...);
+#include <stdarg.h>
+int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+#endif
